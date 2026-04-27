4d04e8e2681a34127244ba12b70be52d40cfda05
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-27 16:01:11 +0200

    Aggiunto footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,60 @@
+function Footer() {
+    return (
+        <footer className="footer-top">
+            <div className="container footer-container">
+                <div className="footer-links">
+                    <div>
+                        <h3>DC COMICS</h3>
+                        <ul>
+                            <li>Characters</li>
+                            <li>Comics</li>
+                            <li>Movies</li>
+                            <li>TV</li>
+                            <li>Games</li>
+                            <li>Videos</li>
+                            <li>News</li>
+                        </ul>
+
+                        <h3>SHOP</h3>
+                        <ul>
+                            <li>Shop DC</li>
+                            <li>Shop DC Collectibles</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>DC</h3>
+                        <ul>
+                            <li>Terms Of Use</li>
+                            <li>Privacy policy</li>
+                            <li>Ad Choices</li>
+                            <li>Advertising</li>
+                            <li>Jobs</li>
+                            <li>Subscriptions</li>
+                            <li>Talent Workshops</li>
+                            <li>CPSC Certificates</li>
+                            <li>Ratings</li>
+                            <li>Shop Help</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>SITES</h3>
+                        <ul>
+                            <li>DC</li>
+                            <li>MAD Magazine</li>
+                            <li>DC Kids</li>
+                            <li>DC Universe</li>
+                            <li>DC Power Visa</li>
+                        </ul>
+                    </div>
+                </div>
+
+                <img className="big-logo" src="/img/dc-logo-bg.png" alt="DC big logo" />
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
