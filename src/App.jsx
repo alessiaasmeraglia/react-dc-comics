53dd061f032fdbc60e5143ce491572b87cba0cb7
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-27 15:55:31 +0200

    Aggiunto sezioni della pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import BlueBanner from './components/BlueBanner'
+import Footer from './components/Footer'
+import FooterBottom from './components/FooterBottom'
 
 function App() {
-
-  return <h1>ciao</h1>
+  return (
+    <>
+      <Header />
+      <Main />
+      <BlueBanner />
+      <Footer />
+      <FooterBottom />
+    </>
+  )
 }
 
 export default App
