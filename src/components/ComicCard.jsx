e869df1a8af6a4f1351eceab49cad72fce9494e8
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-28 15:30:14 +0200

    Creata componente card singola

---
+++ src/components/ComicCard.jsx
@@ -0,0 +1,10 @@
+function ComicCard({ comic }) {
+    return (
+        <article className="comic-card">
+            <img src={comic.thumb} alt={comic.series} />
+            <h3>{comic.series}</h3>
+        </article>
+    )
+}
+
+export default ComicCard
