fd44b629d0da8eda4a9ac03ac8d051ddc843a334
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-04-28 15:28:34 +0200

    Aggiunto array della navbar dentro componente

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,65 @@
 function Header() {
     const navItems = [
-        'CHARACTERS',
-        'COMICS',
-        'MOVIES',
-        'TV',
-        'GAMES',
-        'COLLECTIBLES',
-        'VIDEOS',
-        'FANS',
-        'NEWS',
-        'SHOP',
+        {
+            id: 1,
+            text: 'CHARACTERS',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 2,
+            text: 'COMICS',
+            url: '#',
+            active: true,
+        },
+        {
+            id: 3,
+            text: 'MOVIES',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 4,
+            text: 'TV',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 5,
+            text: 'GAMES',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 6,
+            text: 'COLLECTIBLES',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 7,
+            text: 'VIDEOS',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 8,
+            text: 'FANS',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 9,
+            text: 'NEWS',
+            url: '#',
+            active: false,
+        },
+        {
+            id: 10,
+            text: 'SHOP',
+            url: '#',
+            active: false,
+        },
     ]
 
     return (
